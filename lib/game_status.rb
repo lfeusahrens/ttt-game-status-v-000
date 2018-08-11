a16c835730b236b74b1711f8965d560addeee08b
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-11 18:27:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,6 +36,6 @@ WIN_COMBINATIONS = [
   
   def winner(board)
     winner = won?(board)
-    !(board[index].nil? || board[index] == " ")
+    !(winner[index].nil? || winner[index] == " ")
     
   end
