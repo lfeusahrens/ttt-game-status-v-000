4561effdcaab39ca1bd48d948f8cb50538779993
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-11 15:45:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,5 +18,11 @@ WIN_COMBINATIONS = [
   def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    win_index_4 = win_combination[3]
+    win_index_5 = win_combination[4]
+    win_index_6 = win_combination[5]
+    win_index_7 = win_combination[6]
+    win_index_8 = win_combination[7]
     
   end
